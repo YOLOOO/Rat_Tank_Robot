f634a8bfa0390298c91070e03672bfe2a7dddd58
Viktor Tornborg <viktortornborg@hotmail.com>
2026-04-02 10:47:53 +0200

    blah

--- Rat/common_hardware/__init__.py
+++ Rat/common_hardware/__init__.py
@@ -32,7 +32,7 @@ def get_servo_controller():
     """Get or create the servo controller singleton."""
     global _servo_controller
     if _servo_controller is None:
-        _servo_controller = PigpioServo(SERVO_PCB_VERSION, SERVO_PWM_FREQ)
+        _servo_controller = PigpioServo()
     return _servo_controller
 
 __all__ = [
