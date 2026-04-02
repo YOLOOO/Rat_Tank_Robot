a7f3059966b95ab7bbd5bb63b3f7e2242f471372
Viktor Tornborg <viktortornborg@hotmail.com>
2026-04-02 12:25:09 +0200

    blah

--- Rat/rat_brain/brain_state.py
+++ Rat/rat_brain/brain_state.py
@@ -199,7 +199,7 @@ class RatBrain:
         
         # Flash LED with selection color
         selection_color = self._get_selected_item_color()
-        self.led_controller.set_all_led_rgb(*selection_color)
+        self.led_controller.set_all_led_rgb(selection_color[1])
 
     def _start_behavior(self, behavior_name: str):
         """Start a behavior."""
